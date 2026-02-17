4b24e66c1ffcf16aff4a57dbdcb54a25344e8d46
salindupawan <salindupa@gmail.com>
2026-02-17 15:41:59 +0530

    task: Add input validation using Bean Validation annotations

--- src/main/java/com/project/studentmanagement/entities/Student.java
+++ src/main/java/com/project/studentmanagement/entities/Student.java
@@ -1,6 +1,9 @@
 package com.project.studentmanagement.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,15 +21,20 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "First name is required.")
     @Column(name = "first_name" ,length = 20, nullable = false)
     private String firstName;
 
+    @NotBlank(message = "Last name is required.")
     @Column(name = "last_name" ,length = 20, nullable = false)
     private String lastName;
 
+    @Email(message = "Enter valid email")
     @Column(name = "email" ,length = 100, nullable = false)
     private String email;
 
+    @NotBlank(message = "Phone number is required.")
+    @Size(max = 10, message = "Phone number can not exceed 10 character")
     @Column(name = "phone" ,length = 10, nullable = false)
     private String phone;
 
