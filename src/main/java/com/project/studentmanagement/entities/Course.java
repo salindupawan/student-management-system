ca767444ef21d9f6383ecca24f57737d77a69f38
salindupawan <salindupa@gmail.com>
2026-02-17 14:32:04 +0530

    task: create student and course entity classes

---
+++ src/main/java/com/project/studentmanagement/entities/Course.java
@@ -0,0 +1,28 @@
+package com.project.studentmanagement.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "courses")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column( nullable = false)
+    private String description;
+
+    @ManyToMany(mappedBy = "courses")
+    private List<Student> students;
+}
